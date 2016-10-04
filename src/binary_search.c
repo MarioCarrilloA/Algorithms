4f61e5be6facadc6b1d600e60a39ddcf03c3322a
Mario Alfredo Carrillo Arevalo <mario.carrillo.arevalo@gmail.com>
2016-10-03 22:48:46 -0500

    Calculate num of elements in array
    Signed-off-by: Mario Alfredo Carrillo Arevalo <mario.carrillo.arevalo@gmail.com>

--- src/binary_search.c
+++ src/binary_search.c
@@ -24,14 +24,17 @@ int binary_search(int v[], int num_elements, int element) {
 int main()
 {
 	int v[] = {4, 13, 19, 76, 88, 2, 22};
+	int num_elements;
 
 	printf("Binary Search\n");
-	if (binary_search(v, 7, 76))
+	num_elements = sizeof(v) / sizeof(int);
+
+	if (binary_search(v, num_elements, 76))
 		printf("Found\n");
 	else
 		printf("No found\n");
 
-	if (binary_search(v, 7, 81))
+	if (binary_search(v, num_elements, 81))
 		printf("Found\n");
 	else
 		printf("No found\n");
