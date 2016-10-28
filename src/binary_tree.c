6fd33f618d8f3f936860c964e5c6f98eb764c30d
Mario Alfredo Carrillo Arevalo <mario.carrillo.arevalo@gmail.com>
2016-10-27 22:26:34 -0500

    Add template for binary tree
    Signed-off-by: Mario Alfredo Carrillo Arevalo <mario.carrillo.arevalo@gmail.com>

---
+++ src/binary_tree.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+struct tree {
+	int num;
+	struct tree *left;
+	struct tree *right;
+};
+
+int insert(int num);
+
+int delete()int num;
+
+int preorder(struct tree *root);
+
+int postorder(struct tree *root);
+
+int inorder(struct tree *root);
+
+int main()
+{
+	printf("Binary tree\n");
+
+	return EXIT_SUCCESS;
+}
