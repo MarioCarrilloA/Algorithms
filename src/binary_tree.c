21b7abbfda538cff4609bf574cdd03b946ad677f
Mario Alfredo Carrillo Arevalo <mario.carrillo.arevalo@gmail.com>
2016-10-28 22:14:11 -0500

    Add some validation for insertions in tree
    Signed-off-by: Mario Alfredo Carrillo Arevalo <mario.carrillo.arevalo@gmail.com>

--- src/binary_tree.c
+++ src/binary_tree.c
@@ -8,7 +8,23 @@ struct tree {
 	struct tree *right;
 };
 
-int insert(int num);
+int insert(struct tree *root, int num)
+{
+	struct tree *new_node;
+
+	new_node = (struct tree*)malloc(sizeof(struct tree));
+	if (new_node == NULL)
+		exit(1);
+	memset(new_node, 0, sizeof(struct tree));
+	new_node->num = num;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	if (root == NULL) {
+		root = new_node;
+		return EXIT_SUCCESS;
+	}
+}
 
 int delete(int num);
 
@@ -20,6 +36,7 @@ int inorder(struct tree *root);
 
 int main()
 {
+	struct tree *root = NULL;
 	printf("Binary tree\n");
 
 	return EXIT_SUCCESS;
