3f79de6154e7022e9d1e03a69f8cd88e9d6bfaca
Mario Alfredo Carrillo Arevalo <mario.carrillo.arevalo@gmail.com>
2016-10-29 21:44:47 -0500

    Add insert fucntion (recursive) for binary tree
    Signed-off-by: Mario Alfredo Carrillo Arevalo <mario.carrillo.arevalo@gmail.com>

--- src/binary_tree.c
+++ src/binary_tree.c
@@ -8,22 +8,30 @@ struct tree {
 	struct tree *right;
 };
 
-int insert(struct tree *root, int num)
+int insert(struct tree **root, int num)
 {
 	struct tree *new_node;
 
-	new_node = (struct tree*)malloc(sizeof(struct tree));
-	if (new_node == NULL)
-		exit(1);
-	memset(new_node, 0, sizeof(struct tree));
-	new_node->num = num;
-	new_node->left = NULL;
-	new_node->right = NULL;
+	
+	if ((*root) == NULL) {
+		new_node = (struct tree*)malloc(sizeof(struct tree));
+		if (new_node == NULL)
+			exit(1);
+		memset(new_node, 0, sizeof(struct tree));
+		new_node->num = num;
+		new_node->left = NULL;
+		new_node->right = NULL;
+		(*root) = new_node;
 
-	if (root == NULL) {
-		root = new_node;
 		return EXIT_SUCCESS;
 	}
+
+	if (num < (*root)->num)
+		insert(&(*root)->left, num);
+	else if (num > (*root)->num)
+		insert(&(*root)->right, num);
+	else
+		return EXIT_SUCCESS;
 }
 
 int delete(int num);
